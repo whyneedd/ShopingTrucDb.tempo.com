a4da9596998c23e363072086781d6d1501c30926
Jordane <jordan2offshel@hotmaile.com>
2022-07-04 18:08:37 -0400

    ShopingTrucDb

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
+// Button Du Menu De Recherche 
 
 document.querySelector(".fa-magnifying-glass").onclick = function (){ 
     document.querySelector(".searchShow").classList.toggle("active");
 }
+// Fin Du Button Du Menu De Recherche 
+
+
+// Button Menu De Nagivation Mobile
+
 document.querySelector(".humbger").onclick = function (){ 
     document.querySelector(".humbger").classList.toggle("active");
     document.querySelector(".humbgerMenu").classList.toggle("active");
@@ -11,3 +17,4 @@ document.querySelector(".humbgerMenu").onclick = function(){
     document.querySelector(".humbger").classList.remove("active");
     document.querySelector(".humbgerMenu").classList.remove("active");
 }
+// Fin Du Button Menu De Nagivation Mobile
