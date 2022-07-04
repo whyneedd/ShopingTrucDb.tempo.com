719e2b207740a72209e8153a665a178094911de5
Jordane <jordan2offshel@hotmaile.com>
2022-07-04 16:31:25 -0400

    ShopingTrucDb

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,12 @@
 document.querySelector(".fa-magnifying-glass").onclick = function (){ 
     document.querySelector(".searchShow").classList.toggle("active");
 }
+document.querySelector(".humbger").onclick = function (){ 
+    document.querySelector(".humbger").classList.toggle("active");
+    document.querySelector(".humbgerMenu").classList.toggle("active");
+}
+
+document.querySelector(".humbgerMenu").onclick = function(){
+    document.querySelector(".humbger").classList.remove("active");
+    document.querySelector(".humbgerMenu").classList.remove("active");
+}
